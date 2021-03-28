0e0fcd130f65e220414ee0e563bd221c1334098d
Vsevolod <pavlovskyi.ve@gmail.com>
2021-03-28 12:07:45 +0300

    Add logging

--- Package.swift
+++ Package.swift
@@ -22,14 +22,17 @@ let package = Package(
 
         .package(name: "KeychainWrapper",
                  url: "https://github.com/pavlovskyive/keychain-wrapper",
-                 from: "1.1.0")
+                 from: "1.1.0"),
+        
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git",
+                 .upToNextMajor(from: "1.9.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AuthLayer",
-            dependencies: ["NetworkService", "KeychainWrapper"]),
+            dependencies: ["NetworkService", "KeychainWrapper", "SwiftyBeaver"]),
         .testTarget(
             name: "AuthLayerTests",
             dependencies: ["AuthLayer", "NetworkService", "KeychainWrapper"])
--- Sources/AuthLayer/AuthProvider.swift
+++ Sources/AuthLayer/AuthProvider.swift
@@ -8,6 +8,9 @@
 import Foundation
 import NetworkService
 import KeychainWrapper
+import SwiftyBeaver
+
+let log = SwiftyBeaver.self
 
 public class AuthProvider: Authenticator {
     
@@ -24,6 +27,13 @@ public class AuthProvider: Authenticator {
     public init(networkProvider: NetworkProvider,
                 secureStorage: SecureStorage,
                 config: AuthConfig) {
+        
+        let platform = SBPlatformDestination(appID: "Gw3AJo",
+                                             appSecret: "afxsclzQ9qhnltomqgiu2vxlgc0rqwoc",
+                                             encryptionKey: "cWtgjh7gtqdkhplpwlKvrigmTDwraUof")
+
+        log.addDestination(platform)
+        log.info("Initializing Auth provider...")
 
         self.networkProvider = networkProvider
         self.secureStorage = secureStorage
@@ -36,15 +46,17 @@ public class AuthProvider: Authenticator {
     public func login(credentials: [String: String], then: @escaping TokenResult) {
         
         tempCredentials = credentials
-
+        log.info("Logging in...")
         authService.login(credentials: credentials) { [weak self] result in
             switch result {
             case .success(let token):
+                log.info("Logged in successfully")
                 self?.saveCredentialsToStorage(credentials)
                 self?.handleTokenRetreiving(token: token)
                 then(.success(token))
                 self?.authDelegates.invoke { $0.onLogin() }
             case .failure(let error):
+                log.error(error.localizedDescription)
                 then(.failure(error))
             }
         }
@@ -53,16 +65,20 @@ public class AuthProvider: Authenticator {
     
     public func tryLogin() {
 
+        log.info("Trying to log in with saved credentials...")
         guard let credentialsString = try? secureStorage.get(forKey: "credentials"),
               let credentials = parseString(credentialsString) else {
+            log.info("No credentials saved on device")
             return
         }
 
+        log.info("Found credentials")
         login(credentials: credentials) {_ in}
 
     }
 
     public func logout(then: @escaping ErrorCompletion) {
+        log.info("Logging out...")
         deleteCredentialsFromStorage()
         authDelegates.invoke { $0.onLogout() }
         handleTokenDeletion()
@@ -71,13 +87,16 @@ public class AuthProvider: Authenticator {
 
     public func register(credentials: [String: String], then: @escaping TokenResult) {
 
+        log.info("Trying to register user...")
         authService.register(credentials: credentials) { [weak self] result in
             switch result {
             case .success(let token):
+                log.info("Successfully registered")
                 self?.handleTokenRetreiving(token: token)
                 then(.success(token))
                 self?.authDelegates.invoke { $0.onLogin() }
             case .failure(let error):
+                log.error(error.localizedDescription)
                 then(.failure(error))
             }
         }
@@ -96,7 +115,7 @@ public class AuthProvider: Authenticator {
 extension AuthProvider {
     
     func handleTokenRetreiving(token: String) {
-        
+        log.verbose("Handling token retreiving...")
         loggedIn = true
         
         saveTokenToStorage(token)
@@ -104,7 +123,7 @@ extension AuthProvider {
     }
     
     func handleTokenDeletion() {
-
+        log.verbose("Handling token deletion...")
         loggedIn = false
 
         deleteTokenFromStorage()
@@ -112,22 +131,27 @@ extension AuthProvider {
     }
     
     func setNetworkAuthorization(_ token: String) {
+        log.verbose("Setting token to Network Service...")
         networkProvider.setAuthorization("Bearer \(token)")
     }
     
     func removeNetworkAuthorization() {
+        log.verbose("Removing token to Network Service...")
         networkProvider.clearAuthorization()
     }
     
     func saveTokenToStorage(_ token: String) {
+        log.verbose("Saving token to storage...")
         try? secureStorage.set(token, forKey: "token")
     }
     
     func deleteTokenFromStorage() {
+        log.verbose("Deleting token from storage...")
         try? secureStorage.delete(forKey: "token")
     }
     
     func saveCredentialsToStorage(_ credentials: [String: String]?) {
+        log.verbose("Saving credentials to storage...")
         guard let credentials = credentials,
               let credentialsString = parseCredentials(credentials) else {
             return
@@ -137,6 +161,7 @@ extension AuthProvider {
     }
     
     func deleteCredentialsFromStorage() {
+        log.verbose("Deleting token from storage...")
         try? secureStorage.delete(forKey: "credentials")
     }
     
@@ -144,6 +169,7 @@ extension AuthProvider {
 
         guard let data = try? JSONSerialization.data(withJSONObject: credentials, options: []),
               let string = String(data: data, encoding: .utf8) else {
+            log.error("Could not parse credentials to String")
             return nil
         }
 
@@ -153,6 +179,7 @@ extension AuthProvider {
     func parseString(_ string: String) -> [String: String]? {
         guard let data = string.data(using: .utf8),
               let credentials = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String] else {
+            log.error("Could not parse credentials string to [String: String]")
             return nil
         }
 
