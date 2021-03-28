f9732b76c83f1963e83d34c878f6ea4de4cfa88e
Vsevolod <pavlovskyi.ve@gmail.com>
2021-03-28 04:20:53 +0300

    Saving credentials + Autologin

--- Sources/AuthLayer/AuthProvider.swift
+++ Sources/AuthLayer/AuthProvider.swift
@@ -18,6 +18,8 @@ public class AuthProvider: Authenticator {
     private var secureStorage: SecureStorage
     private var authService: Authenticator
     private var networkProvider: NetworkProvider
+    
+    private var tempCredentials: [String: String]?
 
     public init(networkProvider: NetworkProvider,
                 secureStorage: SecureStorage,
@@ -27,13 +29,18 @@ public class AuthProvider: Authenticator {
         self.secureStorage = secureStorage
         self.authService = AuthService(networkProvider: networkProvider,
                                        config: config)
+        
+        tryLogin()
     }
     
     public func login(credentials: [String: String], then: @escaping TokenResult) {
+        
+        tempCredentials = credentials
 
         authService.login(credentials: credentials) { [weak self] result in
             switch result {
             case .success(let token):
+                self?.saveCredentialsToStorage(credentials)
                 self?.handleTokenRetreiving(token: token)
                 then(.success(token))
                 self?.authDelegates.invoke { $0.onLogin() }
@@ -43,8 +50,20 @@ public class AuthProvider: Authenticator {
         }
 
     }
+    
+    public func tryLogin() {
+
+        guard let credentialsString = try? secureStorage.get(forKey: "credentials"),
+              let credentials = parseString(credentialsString) else {
+            return
+        }
+
+        login(credentials: credentials) {_ in}
+
+    }
 
     public func logout(then: @escaping ErrorCompletion) {
+        deleteCredentialsFromStorage()
         authDelegates.invoke { $0.onLogout() }
         handleTokenDeletion()
         authService.logout { then($0) }
@@ -106,6 +125,38 @@ extension AuthProvider {
     
     func deleteTokenFromStorage() {
         try? secureStorage.delete(forKey: "token")
+    }
+    
+    func saveCredentialsToStorage(_ credentials: [String: String]?) {
+        guard let credentials = credentials,
+              let credentialsString = parseCredentials(credentials) else {
+            return
+        }
+        
+        try? secureStorage.set(credentialsString, forKey: "credentials")
+    }
+    
+    func deleteCredentialsFromStorage() {
+        try? secureStorage.delete(forKey: "credentials")
+    }
+    
+    func parseCredentials(_ credentials: [String: String]) -> String? {
+
+        guard let data = try? JSONSerialization.data(withJSONObject: credentials, options: []),
+              let string = String(data: data, encoding: .utf8) else {
+            return nil
+        }
+
+        return string
+    }
+    
+    func parseString(_ string: String) -> [String: String]? {
+        guard let data = string.data(using: .utf8),
+              let credentials = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String] else {
+            return nil
+        }
+
+        return credentials
     }
     
 }
